562c1d00bb9b5dbbae711d498837ed400644ac23
seniru-ekanayake <ekanayakeseniru0@gmail.com>
2026-03-29 00:49:25 +0530

    Delete backend directory

--- backend/src/main/java/com/wolverhampton/campushub/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wolverhampton.campushub.repository;
-
-import com.wolverhampton.campushub.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(Role.RoleName name);
-}
