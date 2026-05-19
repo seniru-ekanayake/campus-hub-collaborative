8f345a1b8b295eb05bd4a99d8e012817ad45f983
Amashi Aththanagoda <amashi0441@gmail.com>
2026-05-19 22:57:40 +0530

    new updates

--- backend/src/main/java/com/wolverhampton/campushub/security/UserDetailsImpl.java
+++ backend/src/main/java/com/wolverhampton/campushub/security/UserDetailsImpl.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class UserDetailsImpl implements UserDetails {
@@ -27,14 +26,46 @@ public class UserDetailsImpl implements UserDetails {
                 .collect(Collectors.toList());
     }
 
-    public Long getId() { return id; }
-    public String getEmail() { return email; }
+    public Long getId() {
+        return id;
+    }
 
-    @Override public String getUsername() { return username; }
-    @Override public String getPassword() { return password; }
-    @Override public Collection<? extends GrantedAuthority> getAuthorities() { return authorities; }
-    @Override public boolean isAccountNonExpired() { return true; }
-    @Override public boolean isAccountNonLocked() { return true; }
-    @Override public boolean isCredentialsNonExpired() { return true; }
-    @Override public boolean isEnabled() { return true; }
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
